d1d0e14fece2ff72db516d5ba99a737ae53a98c8
moomie1 <rileyk2809@gmail.com>
2024-08-08 21:53:06 -0700

    admin mostly done(could be wrong, idk)

--- booking_app/admin.py
+++ booking_app/admin.py
@@ -4,7 +4,7 @@ from . import models
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     list_display = [
-        'get_first_name', 'get_last_name', 'view_bookings', 'get_email'
+        'get_first_name', 'get_last_name', 'view_bookings', 'get_email', 'profile_rating'
     ]
     ordering = ['first_name', 'last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
@@ -24,6 +24,11 @@ class UserAdmin(admin.ModelAdmin):
     @admin.display(description='Bookings')
     def view_bookings(self, obj):
         return ", ".join([str(booking) for booking in obj.bookings.all()])
+    
+    @admin.display(description='Profile Rating')
+    def profile_rating(self, obj):
+        return obj.profile_rating or "N/A"
+
 
 @admin.register(models.RoomItem)
 class RoomItemAdmin(admin.ModelAdmin):
@@ -43,3 +48,47 @@ class RoomItemAdmin(admin.ModelAdmin):
             f'{updated_count} rooms were successfully updated',
             messages.WARNING
         ) 
+
+@admin.register(models.Booking)  
+class BookingAdmin(admin.ModelAdmin):
+    list_display = [
+        'user', 'time_placed', 'check_in', 'check_out', 'payment_status', 'booking_reference'
+    ]
+    list_filter = ['payment_status', 'time_placed']
+    search_fields = ['user__username', 'booking_reference']
+    ordering = ['-time_placed']
+
+@admin.register(models.RoomBooked)
+class RoomBookedAdmin(admin.ModelAdmin):
+    list_display = ['room', 'booking', 'location', 'time_booked', 'number_of_nights', 'total_cost']
+    search_fields = ['room__title', 'booking__user__username', 'location__city']
+    list_filter = ['time_booked', 'location__city']
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['room', 'user_reviewing', 'rating', 'description']
+    search_fields = ['room__title', 'user_reviewing__username']
+    list_filter = ['rating']
+
+@admin.register(models.Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ['amount_discounted']
+    search_fields = ['amount_discounted']
+
+@admin.register(models.BookingInfo)
+class BookingInfoAdmin(admin.ModelAdmin):
+    list_display = ['guest_count', 'special_requests']
+    search_fields = ['special_requests']
+
+@admin.register(models.EventInfo)
+class EventInfoAdmin(admin.ModelAdmin):
+    list_display = ['event_name', 'event_date', 'room', 'discount']
+    search_fields = ['event_name', 'room__title']
+    list_filter = ['event_date']
+
+@admin.register(models.Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'status', 'timestamp']
+    search_fields = ['user__username', 'stripe_charge_id']
+    list_filter = ['status', 'timestamp']
+    ordering = ['-timestamp']
